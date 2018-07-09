3e612c59f9e1878b64f42a2f66ea99ea0e4f261b
thephez <thephez@users.noreply.github.com>
2018-07-09 16:29:59 -0400

    Bump to Dash Core 0.12.3.2  - Has devnet updates

--- devnet_install_dash.sh
+++ devnet_install_dash.sh
@@ -1,9 +1,9 @@
 # Download / extract Dash Core binaries to home folder
 DIR=$(pwd)
-DCFILE=dashcore-0.12.3.1-x86_64-linux-gnu.tar.gz
+DCFILE=dashcore-0.12.3.2-x86_64-linux-gnu.tar.gz
 
 cd
-curl -L -O https://github.com/dashpay/dash/releases/download/v0.12.3.1/$DCFILE
+curl -L -O https://github.com/dashpay/dash/releases/download/v0.12.3.2/$DCFILE
 
 tar -xvzf $DCFILE
 ls dash*
